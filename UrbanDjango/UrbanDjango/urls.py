ec39612656fbba3045e8aaf603d82efeb2856063
Dmitriy-68 <d73-68@yandex.ru>
2024-12-12 20:41:14 +0300

    Модуль 18 Задание 2 Функциональное и классовое представление

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import func_temp, class_temp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', func_temp),
+    path('class/', class_temp.as_view())
 ]
