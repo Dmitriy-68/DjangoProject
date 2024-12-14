d4f3c5c7e68ca60da601eb38eb2a31668ab4eded
Dmitriy-68 <d73-68@yandex.ru>
2024-12-14 10:47:52 +0300

    Модуль 18 Задание 5 DTL. HTML и Django формы

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task4.views import index, shop, cart
+from task5.views import sign_up_by_html, sign_up_by_django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index),
-    path('shop.html', shop),
-    path('cart.html', cart)
+    path('', sign_up_by_html),
+    path('django_sign_up', sign_up_by_django),
 ]
